2496dee03513931b3f81da7f9e49b243fc4f6c4d
Nikolay <avnnnyr13@gmail.com>
2025-09-16 11:24:59 +0300

    production build fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
+import path from 'path';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    outputFileTracingRoot: path.join(__dirname),
+};
 
 export default withNextIntl(nextConfig);
