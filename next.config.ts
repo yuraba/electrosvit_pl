0fd01c0e2b45562d79e74b04a0383dc4c2acb165
Nikolay <avnnnyr13@gmail.com>
2025-09-16 11:03:09 +0300

    update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
-    // i18n: {
-    //     locales: ['en', 'pl'],
-    //     defaultLocale: 'pl',
-    //     // This is a list of locale domains and the default locale they
-    //     // should handle (these are only required when setting up domain routing)
-    //     // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    //     domains: [
-    //         {
-    //             domain: 'example.com',
-    //             defaultLocale: 'en',
-    //         },
-    //         {
-    //             domain: 'example.nl',
-    //             defaultLocale: 'pl',
-    //         },
-    //     ],
-    // },
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
